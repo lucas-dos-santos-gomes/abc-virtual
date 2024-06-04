9eec3d22e5a28ec38be0d9c999e69a953913d582
Lucas dos Santos Gomes <lucas.gomes@telefonica.com>
2024-06-04 11:47:49 -0300

    Page e layout jsx e Sign

---
+++ src/app/Sign/page.jsx
@@ -0,0 +1,18 @@
+import Header from "@/components/Header";
+
+export default function Signup() {
+  return (
+    <>
+      <Header />
+      <div>
+        <div className="login">
+          <h1>login</h1>
+        </div>
+        
+        <div className="cadastro">
+          <h1>cadastro</h1>
+        </div>
+      </div>
+    </>
+  );
+}
