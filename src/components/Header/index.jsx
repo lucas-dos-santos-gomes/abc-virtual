cce519c2b6734b6ab08fdcd21d3daed49aaa0972
Lucas dos Santos Gomes <lucas.gomes@telefonica.com>
2024-06-04 11:36:16 -0300

    Header iniciado

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,3 +1,11 @@
+import Image from "next/image";
+// import Button from './style';
+
 export default function Header() {
-  return <>Header</>;
+  return (
+    <header>
+      <Image width={100} height={100} src="/svg/logo.svg" alt="logo" />
+      {/* <Button>Clique em mim!</Button> */}
+    </header>
+  );
 }
